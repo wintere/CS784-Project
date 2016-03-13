ae2f537f57d4998fbb4e6e6683d8923e4a81d0df
jalapao <combustible44@gmail.com>
2016-03-13 01:03:33 -0600

    added heuristics to choose from cands

--- stage2.py
+++ stage2.py
@@ -3,18 +3,20 @@ import datrie
 import re
 import string
 import sys
+from collections import defaultdict
 
 brand_trie = datrie.Trie(string.printable)
 falsepositives = 0
 falsenegatives = 0
 truepositives = 0
 truenegatives = 0
+correct_guess_size = 0
 development_set_size = 0
 right = 0
 syn_dict = dict()
 
 # Import the brand name dictionary into our trie object
-with open('big_dict.csv', 'r') as brand_dict_csv_file:
+with open('big_dict.csv', 'r', encoding="latin-1") as brand_dict_csv_file:
     brand_dict_reader = csv.reader(brand_dict_csv_file, delimiter=',', quotechar='"')
     for brand in brand_dict_reader:
         b_name = brand[0]
@@ -56,7 +58,8 @@ with open('stage2_development_set.csv', 'r') as development_set_csv_file:
                 if sub == False:
                     if c in syn_dict:
                         final_cands.add(syn_dict[c])
-                    final_cands.add(c)
+                    else:
+                        final_cands.add(c)
             #print(cand)
             if len(cand) > 0:
                 cands.extend(final_cands)
@@ -64,7 +67,27 @@ with open('stage2_development_set.csv', 'r') as development_set_csv_file:
         # if brand_trie.values(brand_nae_candidate.lower()):
         # 	print("Using candidate: \"" + brand_name_candidate + "\"")
 
-        # TODO: write heuristic that pulls out correct candidate
+        # Heuristic that pulls out correct candidate
+        chosen = ""
+        candindex = defaultdict(list)
+        lower_name = product_name.lower()
+        for candidate in cands:
+            index = lower_name.find(candidate.lower())
+            candindex[index].append(candidate)
+        if len(candindex) > 0:
+            min_index = min(candindex)
+            chosen = candindex[min_index][0]
+            for candidate in candindex[min_index]:
+                if len(candidate) > len(chosen):
+                    chosen = candidate
+        print("Chosen: ", chosen)
+        
+        if chosen == product[1]:
+            correct_guess_size += 1
+        else:
+            print(product_name)
+            print("Wrong Guess:", chosen, "Brand Name:", product[1])
+
         if (product[1] in cands):
             # print("Correct brand is somewhere in candidates!")
             truepositives += 1
@@ -82,7 +105,9 @@ with open('stage2_development_set.csv', 'r') as development_set_csv_file:
             print("guessed the wrong brand name")
 
 # 	break
+precision = correct_guess_size/float(development_set_size)
 print("\n\n")
+print("Correct Extraction:", correct_guess_size, "Precision:", precision)
 print("True positives:", truepositives, "True negatives", truenegatives, "False positives", falsepositives, "False negatives", falsenegatives)
 print("Overall ", right, "right")
 
