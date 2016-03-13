88fcb253e873da3d4587710b253cfcf27d3f4b27
ERIN <wintere@carleton.edu>
2016-03-13 15:27:32 -0500

    tweaks to trie, fixed mislabelling on brand name data

--- stage2.py
+++ stage2.py
@@ -6,13 +6,10 @@ import sys
 from collections import defaultdict
 
 brand_trie = datrie.Trie(string.printable)
-falsepositives = 0
-falsenegatives = 0
-truepositives = 0
-truenegatives = 0
-correct_guess_size = 0
-development_set_size = 0
-right = 0
+true_positive = 0
+true_negative = 0
+false_positive = 0
+false_negative = 0
 syn_dict = dict()
 
 # Import the brand name dictionary into our trie object
@@ -20,10 +17,12 @@ with open('big_dict.csv', 'r', encoding="latin-1") as brand_dict_csv_file:
     brand_dict_reader = csv.reader(brand_dict_csv_file, delimiter=',', quotechar='"')
     for brand in brand_dict_reader:
         b_name = brand[0]
+        b_name = b_name.title()
         #add variations to dictionary
         #possibly remove
         smushed = ''.join(b_name.split(' '))
-        permutations = [b_name.upper(), b_name.title(), b_name.lower(), smushed, smushed.lower(), smushed.title()]
+        dehyphen = ''.join(b_name.split('-'))
+        permutations = [smushed.upper(), b_name.upper(), dehyphen.upper()]
         for permutation in permutations:
             brand_trie[permutation] = int(brand[1])
             syn_dict[permutation] = b_name
@@ -36,9 +35,8 @@ with open('stage2_development_set.csv', 'r') as development_set_csv_file:
     development_set_reader = csv.reader(development_set_csv_file, delimiter=',', quotechar='"')
     # For each product, try to identify the brand name
     for product in development_set_reader:
-        development_set_size += 1
         # First attempt: split the product name into an array of strings.
-        product_name = product[0]
+        product_name = product[0].upper()
         substrings = product_name.split(' ')
         s_array = []
         for i in range(len(substrings) - 1):
@@ -64,9 +62,6 @@ with open('stage2_development_set.csv', 'r') as development_set_csv_file:
             if len(cand) > 0:
                 cands.extend(final_cands)
         print("Candidates: ", cands)
-        # if brand_trie.values(brand_nae_candidate.lower()):
-        # 	print("Using candidate: \"" + brand_name_candidate + "\"")
-
         # Heuristic that pulls out correct candidate
         chosen = ""
         candindex = defaultdict(list)
@@ -81,37 +76,25 @@ with open('stage2_development_set.csv', 'r') as development_set_csv_file:
                 if len(candidate) > len(chosen):
                     chosen = candidate
         print("Chosen: ", chosen)
-        
-        if chosen == product[1]:
-            correct_guess_size += 1
-        else:
-            print(product_name)
-            print("Wrong Guess:", chosen, "Brand Name:", product[1])
 
-        if (product[1] in cands):
-            # print("Correct brand is somewhere in candidates!")
-            truepositives += 1
-            right += 1
-        elif (product[1] == '' and cands == []):
-            truenegatives += 1
-            right += 1
-        elif  (product[1] == '' and cands != []):
-            print("False POS")
-            falsepositives += 1
-        elif (product[1] != '' and cands == []):
-            print("False NEG: ", product[1], "correct")
-            falsenegatives += 1
+        label = product[1].title()
+        if chosen != '':
+            if chosen == label:
+                true_positive += 1
+            elif label != '':
+                print("WRONG BRAND")
+            else:
+                print("FALSE POS")
+                false_positive += 1
         else:
-            print("guessed the wrong brand name")
-
-# 	break
-precision = correct_guess_size/float(development_set_size)
+            if label == "":
+                true_negative += 1
+            else:
+                false_negative += 1
+                print("FALSE NEG", label)
 print("\n\n")
-print("Correct Extraction:", correct_guess_size, "Precision:", precision)
-print("True positives:", truepositives, "True negatives", truenegatives, "False positives", falsepositives, "False negatives", falsenegatives)
-print("Overall ", right, "right")
+print("True Pos",true_positive, "False Pos",false_positive, "True Neg",true_negative, "False Neg", false_negative)
+precision = float (true_positive)/(true_positive + false_positive)
+recall = float(true_positive)/(true_positive + false_negative)
+print (precision, recall)
 
-# Output our results
-# print("FINAL RESULTS:")
-# print("Correctly identified " + str(correct_brand_name_candidates) + " brand names from a set of " + str(development_set_size) + " entries")
-# print("Accuracy: " + str(correct_brand_name_candidates*100/development_set_size) + "%")
